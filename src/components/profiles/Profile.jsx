18eea7b91069f539d7e68df38ae7c7c0f3389e12
Thomas Stussi <tstussi06@gmail.com>
2020-11-03 14:19:23 -0800

    more profile component

--- src/components/profiles/Profile.jsx
+++ src/components/profiles/Profile.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable keyword-spacing */
+/* eslint-disable max-len */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUser, setSearch } from '../../actions/githubActions';
+import { fetchUser, setLoading, setSearch } from '../../actions/githubActions';
 import ProfileSearch from './ProfileSearch';
 
 const Profile = () => {
@@ -9,23 +11,38 @@ const Profile = () => {
   const dispatch = useDispatch();
 
   const handleSubmit = userName => {
-    dispatch(fetchUser(userName))
-  }
+    dispatch(setLoading(true));
+    dispatch(fetchUser(userName));
+  };
 
   const handleChange = ({ target }) => {
-    dispatch(setSearch(target.value))
-  }
+    dispatch(setSearch(target.value));
+  };
 
-  if(loading) return <h1>Loading...</h1>;
+  const repoElements = user.userRepos.map(repo => (
+    <li key={repo.name}>
+      <p>Name: {repo.name}</p>
+      <a href={repo.url}>Link: {repo.url}</a>
+    </li>
+  ));
 
-  if(user === {}) return <ProfileSearch onChange={} onSubmit={} />;
+  if (loading) return <h1>Loading...</h1>;
+
+  if (user === {}) return <ProfileSearch onChange={handleChange} onSubmit={handleSubmit} />;
 
   return (
     <>
-    <ProfileSearch onChange={handleChange} onSubmit={handleSubmit} />
-
+      <ProfileSearch onChange={handleChange} onSubmit={handleSubmit} />
+      <h1>{user.userProfile.name}</h1>
+      <a href={user.userProfile.url}>Link: {user.userProfile.url}</a>
+      <p>Follower Count: {user.userProfile.followers}</p>
+      <p>Following {user.userProfile.followers} Users</p>
+      <ul>
+        Repositories:
+        {repoElements}
+      </ul>
     </>
-  )
-}
+  );
+};
 
 export default Profile;
