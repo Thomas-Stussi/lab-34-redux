83f220f6e9a2fd40093f961986edf4cf825a8a85
Thomas Stussi <tstussi06@gmail.com>
2020-11-03 16:01:38 -0800

    ProfileSearch test

--- src/components/profiles/ProfileSearch.test.jsx
+++ src/components/profiles/ProfileSearch.test.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import App from './App';
+import ProfileSearch from './ProfileSearch';
 import { Provider } from 'react-redux';
 import store from '../../store';
 
-describe('App component', () => {
+describe('ProfileSearch component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
+  it('renders ProfileSearch', () => {
     const { asFragment } = render(
       <Provider store={store}>
-        <App />
-      </Provider>);
+        <ProfileSearch />
+      </Provider>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
